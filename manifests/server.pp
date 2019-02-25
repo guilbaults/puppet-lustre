decaef277e320251be34a51a25d5e0da6ba1c61c
Simon Guilbault <sigui4@puppet1.int.ets1.calculquebec.ca>
2019-02-25 14:38:47 +0000

    network interface checks are done in a external script, that script put the node in standby if there are some errors

--- manifests/server.pp
+++ manifests/server.pp
@@ -126,21 +126,6 @@ echo $password
     }
   }
 
-#  cs_primitive { 'ib0-healthy':
-#    primitive_class => 'ocf',
-#    primitive_type  => 'ethmonitor',
-#    provided_by     => 'heartbeat',
-#    parameters      => { 'interface' => 'ib0' ,'infiniband_device' => 'mlx5_0' },
-#    operations      => {
-#      'monitor' => { 'timeout' => '30s' , 'interval' => '30s' },
-#    },
-#  }
-#  cs_clone { 'ib0-healthy-clone' :
-#    ensure    => present,
-#    primitive => 'ib0-healthy',
-#    require   => Cs_primitive['ib0-healthy'],
-#  }
-
   cs_rsc_defaults { 'resource-stickiness' :
     value => '100',
   }
